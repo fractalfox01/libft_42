57bac399a15b5e20b0908c75a401bc782c5087ff
fractalfox01 <fractalfox01@gmail.com>
2019-01-19 15:49:11 -0700

    adding test to memmove.

--- test/memmove_test.c
+++ test/memmove_test.c
@@ -3,10 +3,12 @@
 #include <string.h>
 #include "../include/libft.h"
 
-// SEGFAULT in program somewhere.
-
 int     main(void)
 {
+	int i = 0;
+	int arr[10] = {12, 23, 34, 45, 56, 67, 78, 89, 90, 101};
+	int arr2[10] = {102, 91, 90, 79, 68, 57, 46, 35, 24, 13};
+	int *arr3;
 	char tmp[22] = "g Simple Test gtring.";
         const char strsrc[22] = "A Simple Test String.";
         char *strdest;
@@ -16,11 +18,20 @@ int     main(void)
         char *str2;
 
         strdest = (char *)malloc(sizeof(char) * sizeof(strsrc));
+	arr3 = (int *)malloc(sizeof(int) * 10);
 
         printf("Original tmp: %s\nOriginal strsrc: %s\nOriginal strsrc2: %s\nstrdest is malloc'd sizeof(strsrc) = 22\n\n", tmp, strsrc, strsrc2);
         str = ft_memmove(strdest, strsrc, (size_t)8);
         str2 = ft_memmove(tmp, strsrc2, (size_t)8);
 	printf("strdest is dest && strsrc is src. n is 8\noutput: %s\n\n", str);
         printf("tmp as dest && strsrc2 as src. n is 8\noutput: %s\n", str2);
+
+	printf("\ntrying with array of 10 integers.\n");
+	arr3 = ft_memmove(arr2, arr, (size_t)(sizeof(arr)/2));
+	while (i < 10)
+	{
+		printf("%d\n", arr3[i]);
+		i++;
+	}
         return (0);
 }
