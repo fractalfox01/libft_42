3bceb55b965133b30de296cf346ebed14b20164a
fractalfox01 <fractalfox01@gmail.com>
2019-02-03 18:28:56 -0700

    adding strcmp w/test

--- src/ft_strcmp.c
+++ src/ft_strcmp.c
@@ -1,32 +1,50 @@
-#include <stdio.h>
-
-int	strnum(char *str)
-{
-	long int	nbr;
-	int		i;
-	nbr = 0;
-	i = 0;
-	while (str[i] != 0)
-	{
-		if (str[i] == str[i + 1] && str[i + 1] != 0)
-		{
-			nbr += str[i];
-			i++;
-			while (str[i] == str[i + 1] && str[i + 1] != 0)
-				i++;
-		}
-		nbr += *str;
-		str++;
-	}
-	return (nbr);
-}
+#include "../include/libft.h"
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	nb1;
-	int	nb2;
+//	int	i;
+//	int	nbr1;
+//	int	nbr2;
+//	int	len1;
+//	int	len2;
+	int	ans;
 
-	nb1 = strnum(s1);
-	nb2 = strnum(s2);
-	return (nb1 - nb2);
+//	nbr1 = 0;
+//	nbr2 = 0;
+	ans = (int)*s1 - (int)*s2;
+//	len1 = ft_strlen(s1);
+//	len2 = ft_strlen(s2);
+//	i = 0;
+	if (!(*s1) || !(*s2))
+		return (-1);
+	while (*s1 == *s2 && ((*s1 != '\0') && (*s2 != '\0')))
+	{
+		s1++;
+		s2++;
+		ans = (int)(*s1) - (int)(*s2);
+	}
+//	while (i < len1)
+//	{
+//		if (i > 0)
+//		{
+//			if (s1[i - 1] != s1[i])
+//				nbr1 += (int)(s1[i]);
+//		}
+//		else if (i == 0)
+//			nbr1 += (int)(s1[i]);
+//		i++;
+//	}
+//	i = 0;
+//	while (i < len2)
+//	{
+//		if (i > 0)
+//		{
+//			if (s2[i - 1] != s2[i])
+//				nbr2 += (int)(s2[i]);
+//		}
+//		else if (i == 0)
+//			nbr2 += (int)(s2[i]);
+//		i++;
+//	}
+	return (ans);
 }
--- test/strcmp_test.c
+++ test/strcmp_test.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include "../include/libft.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	int i = 0;
 	char str1[9] = "atoms\0\0\0";
 	char str2[9] = "atoms\0a\0";
 	char str3[5] = "\v\r\n\t";
@@ -16,42 +17,88 @@ int	main(void)
 	char str10[7] = "aaatom";
 	char str11[3] = " k";
 	char str12[3] = " b";
-	char str13[5] = "aaaa";
+	char str13[5] = "AAAA";
 	char str14[5] = "    ";
-	char str15[9] = "kkkk    ";
+	char str15[9] = "KKKK    ";
 	char str16[9] = "        ";
 
-	printf("\"atoms\\0\\0\\0\" vs \"atoms\\0a\\0\"\n");
-	printf("Mine: %d\n", ft_strcmp(str1, str2));
-	printf("Their's: %d\n", strcmp(str1, str2));
+	if (argc == 1)
+	{
+		if (ft_strcmp(str1, str2) == strcmp(str1, str2))
+		{
+			i++;
+			if (ft_strcmp(str3, str4) == strcmp(str3, str4))
+			{
+				i++;
+				if (ft_strcmp(str5, str7) == strcmp(str5, str7))
+				{
+					i++;
+					if (ft_strcmp(str6, str8) == strcmp(str6, str8))
+					{
+						i++;
+						if (ft_strcmp(str9, str10) == strcmp(str9, str10))
+						{
+							i++;
+							if (ft_strcmp(str11, str12) == strcmp(str11, str12))
+							{
+								i++;
+								if (ft_strcmp(str13, str14) == strcmp(str13, str14))
+								{
+									i++;
+									if (ft_strcmp(str15, str16) == strcmp(str15, str16))
+									{
+										printf("All tests pass\n");
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		if (i < 7)
+		{
+			printf("Failed at test #%d\n", (i + 1));
+		}
+	}
+	if (argc > 1)
+	{
+		if (strcmp(argv[1], "-v") == 0)
+		{
+			printf("\"atoms\\0\\0\\0\" vs \"atoms\\0a\\0\"\n");
+			printf("Mine: %d\n", ft_strcmp(str1, str2));
+			printf("Their's: %d\n", strcmp(str1, str2));
 
-	printf("\n\"\\v\\r\\n\\t\" vs \"%s\"\n", str4);
-	printf("Mine: %d\n", ft_strcmp(str3, str4));
-	printf("Their's: %d\n", strcmp(str3, str4));
+			printf("\n\"\\v\\r\\n\\t\" vs \"%s\"\n", str4);
+			printf("Mine: %d\n", ft_strcmp(str3, str4));
+			printf("Their's: %d\n", strcmp(str3, str4));
 
-	printf("\n\"%s\" vs \"%s\"\n", str5, str7);
-	printf("Mine: %d\n", ft_strcmp(str5, str7));
-	printf("Their's: %d\n", strcmp(str5, str7));
+			printf("\n\"%s\" vs \"%s\"\n", str5, str7);
+			printf("Mine: %d\n", ft_strcmp(str5, str7));
+			printf("Their's: %d\n", strcmp(str5, str7));
 
-	printf("\n\"%s\" vs \"%s\"\n", str6, str8);
-	printf("Mine: %d\n", ft_strcmp(str6, str8));
-	printf("Their's: %d\n", strcmp(str6, str8));
+			printf("\n\"%s\" vs \"%s\"\n", str6, str8);
+			printf("Mine: %d\n", ft_strcmp(str6, str8));
+			printf("Their's: %d\n", strcmp(str6, str8));
 
-	printf("\n\"%s\" vs \"%s\"\n", str9, str10);
-	printf("Mine: %d\n", ft_strcmp(str9, str10));
-	printf("Their's: %d\n", strcmp(str9, str10));
+			printf("\n\"%s\" vs \"%s\"\n", str9, str10);
+			printf("Mine: %d\n", ft_strcmp(str9, str10));
+			printf("Their's: %d\n", strcmp(str9, str10));
 
-	printf("\n\"%s\" vs \"%s\"\n", str11, str12);
-	printf("Mine: %d\n", ft_strcmp(str11, str12));
-	printf("Their's: %d\n", strcmp(str11, str12));
+			printf("\n\"%s\" vs \"%s\"\n", str11, str12);
+			printf("Mine: %d\n", ft_strcmp(str11, str12));
+			printf("Their's: %d\n", strcmp(str11, str12));
 
-	printf("\n\"%s\" vs \"%s\"\n", str13, str14);
-	printf("Mine: %d\n", ft_strcmp(str13, str14));
-	printf("Their's: %d\n", strcmp(str13, str14));
+			printf("\n\"%s\" vs \"%s\"\n", str13, str14);
+			printf("Mine: %d\n", ft_strcmp(str13, str14));
+			printf("Their's: %d\n", strcmp(str13, str14));
 
-	printf("\n\"%s\" vs \"%s\"\n", str15, str16);
-	printf("Mine: %d\n", ft_strcmp(str15, str16));
-	printf("Their's: %d\n", strcmp(str15, str16));
-
+			printf("\n\"%s\" vs \"%s\"\n", str15, str16);
+			printf("Mine: %d\n", ft_strcmp(str15, str16));
+			printf("Their's: %d\n", strcmp(str15, str16));
+		}
+		else
+			printf("Bad option.\n");
+	}
 	return (0);
 }
