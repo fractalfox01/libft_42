5965e5ed1e990a9f11e2a4ed8fb30d72bcb93d9c
Thomas Vandivier <tvandivi@e1z3r7p25.42.us.org>
2019-02-18 01:55:42 -0800

    adding ft_strjoin

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: tvandivi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 20:09:59 by tvandivi          #+#    #+#             */
-/*   Updated: 2019/02/17 22:51:25 by tvandivi         ###   ########.fr       */
+/*   Updated: 2019/02/18 01:23:36 by tvandivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_atoi(char *str);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
