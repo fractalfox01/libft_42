863005434b4bb806c895077b54b16c782ddac9a4
Thomas Vandivier <tvandivi@e1z2r5p2.42.us.org>
2019-03-29 18:13:23 -0700

    updating

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: tvandivi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 20:09:59 by tvandivi          #+#    #+#             */
-/*   Updated: 2019/03/10 19:10:55 by tvandivi         ###   ########.fr       */
+/*   Updated: 2019/03/29 18:11:24 by tvandivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,24 @@
 
 # include <string.h>
 # include <stdlib.h>
+# include <unistd.h>
+
+# define FT_BLACK		"\033[0;30m"
+# define FT_RED			"\033[0;31m"
+# define FT_GREEN		"\033[0;32m"
+# define FT_BROWN		"\033[0;33m"
+# define FT_BLUE		"\033[0;34m"
+# define FT_PURPLE		"\033[0;35m"
+# define FT_CYAN		"\033[0;36m"
+# define FT_LIGHTGRAY	"\033[0;37m"
+# define FT_DARKGRAY	"\033[1;30m"
+# define FT_LIGHT_RED	"\033[1;31m"
+# define FT_LIGHTGREEN	"\033[1;32m"
+# define FT_YELLOW		"\033[1;33m"
+# define FT_LIGHTBLUE	"\033[1;34m"
+# define FT_LIGHTPURPLE	"\033[1;35m"
+# define FT_LIGHTCYAN	"\033[1;36m"
+# define FT_WHITE		"\033[1;37m"
 
 typedef struct		s_list
 {
