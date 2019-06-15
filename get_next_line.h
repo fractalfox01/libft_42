f546aaa05d4784e2aa3dabbca22dc2f81b113849
Thomas Vandivier <tvandivi@e1z2r8p31.42.us.org>
2019-06-15 13:01:28 -0700

    adding get_next_line.h and main.txt(used for testing)

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvandivi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/22 18:39:57 by tvandivi          #+#    #+#             */
+/*   Updated: 2019/03/29 13:41:42 by tvandivi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE	42
+# define FD_LIMIT	4891
+
+int	get_next_line(const int fd, char **line);
+int	get_n_char(const int fd, char **line, int BUF_SIZE);
+
+#endif
